c480c9361ddedbc9d6d210396db7734f2eeeffa6
Atifah Nabilla Al Qibtiyah <atifah.nabilla@ui.ac.id>
2022-01-01 00:54:41 +0700

    drawer logout lumayan aman

--- lib/profil/drawer.dart
+++ lib/profil/drawer.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'profile_page.dart';
-import 'edit_profil.dart';
+import 'package:provider/provider.dart';
+import 'package:helpink_u/common/network_service.dart';
+import 'edit_profile.dart';
+import 'package:helpink_u/login/login.dart';
+import 'package:helpink_u/main.dart';
+import 'package:http/http.dart' as http;
+
 
 class DrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<NetworkService>();
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -29,19 +36,33 @@ class DrawerWidget extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => const AddProfile()));
             },
           ),
-          ListTile('Logout', Icons.login, () async {
-            final response = await request
-                .logoutAccount("https://127.0.0.1:8000/login/logoutFlutter");
-            if (response['status']) {
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const PageLogin()));
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text("An error occured, please try again."),
-              ));
-            }
-          }),
+          const SizedBox(height: 16),
+            request.loggedIn
+                ? ListTile(
+                    title: const Text('Log Out'),
+                    onTap: () async {
+                      final response = await request.logoutAccount(
+                          "https://127.0.0.1:8000/login/logoutFlutter");
+                      if (response['status']) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Successfully logged out!"),
+                        ));
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => LoginPage(),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("An error occured, please try again."),
+                        ));
+                      }
+                    },
+                  )
+
         ],
       ),
     );
