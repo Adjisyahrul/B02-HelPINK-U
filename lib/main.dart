ae79b0d822eff59a18b34acf5f8c8ec2c4e38a74
Atifah Nabilla Al Qibtiyah <atifah.nabilla@ui.ac.id>
2022-01-01 00:29:51 +0700

    tambahin drawer di main

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 import 'saran/main_form_saran.dart';
 import 'saran/main_form_saran.dart';
 import 'login/login.dart';
-
+import 'profil/drawer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-      return Provider(
+    return Provider(
       create: (_) {
         NetworkService request = NetworkService();
 
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key,  this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -45,51 +45,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
-      appBar: AppBar(
-        title: Text(widget.title, style: const TextStyle(color: myColor2)),
-      ),
-      body: WelcomeScreen(),//CarouselHome()HomeDonatur()FormSaran()KritikAdmin()HomePengaju()SaranProvide()HomeAdmin()SaranProvide()FormProvide()LoginPage()FormProvide()
-      backgroundColor: myColor2
-    );
+        drawer: DrawerWidget(),
+        appBar: AppBar(
+          title: Text(widget.title, style: const TextStyle(color: myColor2)),
+        ),
+        body:
+            WelcomeScreen(), //CarouselHome()HomeDonatur()FormSaran()KritikAdmin()HomePengaju()SaranProvide()HomeAdmin()SaranProvide()FormProvide()LoginPage()FormProvide()
+        backgroundColor: myColor2);
   }
 }
 
-const MaterialColor myColor1 =  MaterialColor(
-    0xFFC37B89,
-    <int, Color>{
-      50: Color(0xFFC37B89),
-      100: Color(0xFFC37B89),
-      200: Color(0xFFC37B89),
-      300: Color(0xFFC37B89),
-      400: Color(0xFFC37B89),
-      500: Color(0xFFC37B89),
-      600: Color(0xFFC37B89),
-      700: Color(0xFFC37B89),
-      800: Color(0xFFC37B89),
-      900: Color(0xFFC37B89),
-    }
-);
+const MaterialColor myColor1 = MaterialColor(0xFFC37B89, <int, Color>{
+  50: Color(0xFFC37B89),
+  100: Color(0xFFC37B89),
+  200: Color(0xFFC37B89),
+  300: Color(0xFFC37B89),
+  400: Color(0xFFC37B89),
+  500: Color(0xFFC37B89),
+  600: Color(0xFFC37B89),
+  700: Color(0xFFC37B89),
+  800: Color(0xFFC37B89),
+  900: Color(0xFFC37B89),
+});
 //const SecondaryColor =  Color(0xFFF3F4ED);
-const MaterialColor myColor2 =  MaterialColor(
-    0xFFF3F4ED,
-    <int, Color>{
-      50: Color(0xFFF3F4ED),
-      100: Color(0xFFF3F4ED),
-      200: Color(0xFFF3F4ED),
-      300: Color(0xFFF3F4ED),
-      400: Color(0xFFF3F4ED),
-      500: Color(0xFFF3F4ED),
-      600: Color(0xFFF3F4ED),
-      700: Color(0xFFF3F4ED),
-      800: Color(0xFFF3F4ED),
-      900: Color(0xFFF3F4ED),
-    }
-);
+const MaterialColor myColor2 = MaterialColor(0xFFF3F4ED, <int, Color>{
+  50: Color(0xFFF3F4ED),
+  100: Color(0xFFF3F4ED),
+  200: Color(0xFFF3F4ED),
+  300: Color(0xFFF3F4ED),
+  400: Color(0xFFF3F4ED),
+  500: Color(0xFFF3F4ED),
+  600: Color(0xFFF3F4ED),
+  700: Color(0xFFF3F4ED),
+  800: Color(0xFFF3F4ED),
+  900: Color(0xFFF3F4ED),
+});
 
 // void main() => runApp(MyApp());
 
